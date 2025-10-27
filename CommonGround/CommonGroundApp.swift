5d2cdd1da05b37ae28cebd346dc1ebc4034f98c2
bo.lindsey <bo.lindsey750@gmail.com>
2025-10-27 07:17:12 -0500

    Initial Commit

---
+++ CommonGround/CommonGroundApp.swift
@@ -0,0 +1,17 @@
+//
+//  CommonGroundApp.swift
+//  CommonGround
+//
+//  Created by Bo Lindsey on 10/27/25.
+//
+
+import SwiftUI
+
+@main
+struct CommonGroundApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
