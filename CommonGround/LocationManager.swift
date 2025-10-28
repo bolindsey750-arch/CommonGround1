6f1744670b8f3b8f83b76d769e8602be044e7e8e
bo.lindsey <bo.lindsey750@gmail.com>
2025-10-28 18:11:04 -0500

    yo

--- CommonGround/LocationManager.swift
+++ CommonGround/LocationManager.swift
@@ -6,30 +6,47 @@ class LocationManager: NSObject, ObservableObject, CLLocationManagerDelegate {
     private let manager = CLLocationManager()
 
     @Published var userLocation: CLLocationCoordinate2D?
+    @Published var authorizationStatus: CLAuthorizationStatus
 
     override init() {
+        self.authorizationStatus = manager.authorizationStatus
         super.init()
+
         manager.delegate = self
         manager.desiredAccuracy = kCLLocationAccuracyBest
 
-        // Ask for permission. We don't start updating yet.
+        // Request permission
         manager.requestWhenInUseAuthorization()
+
+        // For iOS versions where the delegate callback doesn’t fire immediately:
+        checkAuthorizationAndStartIfPossible()
     }
 
-    // Called when permission changes (or right away on launch if already granted)
-    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+    private func checkAuthorizationAndStartIfPossible() {
         switch manager.authorizationStatus {
         case .authorizedWhenInUse, .authorizedAlways:
             manager.startUpdatingLocation()
         default:
-            // not allowed yet (denied, restricted, notDetermined)
             break
         }
     }
 
-    // Called whenever we actually get a location
+    // MARK: - CLLocationManagerDelegate
+
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        authorizationStatus = manager.authorizationStatus
+        checkAuthorizationAndStartIfPossible()
+    }
+
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        userLocation = locations.last?.coordinate
+        if let coordinate = locations.last?.coordinate {
+            DispatchQueue.main.async {
+                self.userLocation = coordinate
+            }
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print("Location error: \(error.localizedDescription)")
     }
 }
-
